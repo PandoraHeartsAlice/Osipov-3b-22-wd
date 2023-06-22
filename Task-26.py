5f29efcb27118968586e4b5421fc0453a22b50b4
Osipov <osipov.mixail.2003@mail.ru>
2023-06-22 19:18:47 +0300

    Task-26

---
+++ Task-26.py
@@ -0,0 +1,14 @@
+# наибольший* общий делитель
+
+def find_gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+gcd = find_gcd(num1, num2)
+
+print("The greatest common divisor of", num1, "and", num2, "is:", gcd)
