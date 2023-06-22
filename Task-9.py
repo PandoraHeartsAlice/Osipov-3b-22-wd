e68c799d8edb67c304a45268d59cb8cb14e297cc
Osipov <osipov.mixail.2003@mail.ru>
2023-06-22 16:01:09 +0300

    Task-9

--- Task-9.py
+++ Task-9.py
@@ -1,3 +1,11 @@
-import my_module
+class Rectangle:
+    def __init__(self, height, width):
+        self.width = width
+        self.height = height
 
-my_module.Hello()
+    def area(self):
+        print("area =", self.width*self.height)
+
+
+rectangle = Rectangle(7, 3)
+print(rectangle.area())
