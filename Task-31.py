cca7d5fa27cce7a5c222e572142ad0e9ceeac7f0
Osipov <osipov.mixail.2003@mail.ru>
2023-06-22 20:09:37 +0300

    Task-31

---
+++ Task-31.py
@@ -0,0 +1,13 @@
+import calculator
+
+result = calculator.add(5, 10)
+print(result)  # 15
+
+result = calculator.subtract(20, 7)
+print(result)  # 13
+
+result = calculator.multiply(3, 4)
+print(result)  # 12
+
+result = calculator.divide(10, 2)
+print(result)  # 5.0
